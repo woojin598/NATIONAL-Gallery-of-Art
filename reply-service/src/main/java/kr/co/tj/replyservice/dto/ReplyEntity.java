ba5a7ebde2292d1a84a24123e7943ebd0c8eba42
tj705 <woojin598@nate.com>
2023-06-09 18:38:41 +0900

    reply 중간점검

--- reply-service/src/main/java/kr/co/tj/replyservice/dto/ReplyEntity.java
+++ reply-service/src/main/java/kr/co/tj/replyservice/dto/ReplyEntity.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.ColumnDefault;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -41,5 +43,10 @@ public class ReplyEntity implements Serializable{
 	private Date createDate;
 	
 	private Date updateDate;
+	
+	@ColumnDefault("'ROLE_USER'")
+	private String role;
+	
+
 
 }
--- reply-service/src/main/java/kr/co/tj/replyservice/service/ReplyService.java
+++ reply-service/src/main/java/kr/co/tj/replyservice/service/ReplyService.java
@@ -1,9 +1,15 @@
 package kr.co.tj.replyservice.service;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import kr.co.tj.replyservice.dto.ReplyDTO;
@@ -22,10 +28,12 @@ public class ReplyService {
 		replyDTO = getDate(replyDTO);
 		
 		ReplyEntity replyEntity = replyDTO.toReplyEntity();
+		replyEntity.setRole("ROLE_USER");
 		
 		replyEntity = replyRepository.save(replyEntity);
 		
-		return replyDTO;
+		
+		return replyDTO.toReplyDTO(replyEntity);
 	}
 
 	private ReplyDTO getDate(ReplyDTO replyDTO) {
@@ -40,21 +48,13 @@ public class ReplyService {
 		return replyDTO;
 	}
 
-	public ReplyDTO findById(long id) {
-	    Optional<ReplyEntity> replyEntityOptional = replyRepository.findById(id);
-	    
-	    if (replyEntityOptional.isPresent()) {
-	        return ReplyDTO.toReplyEntity(replyEntityOptional.get());
-	    }
-	    
-	    return null;
-	}
+
 	public ReplyDTO update(ReplyDTO dto) {
 		Optional<ReplyEntity> optional = replyRepository.findById(dto.getId());
 		if (optional.isPresent()) {
 			ReplyEntity entity = optional.get();
 			entity.setComment(dto.getComment());
-			entity.setUsername(dto.getUsername());
+			entity.setId(dto.getId());
 			entity.setUpdateDate(new Date());
 			entity = replyRepository.save(entity);
 			return ReplyDTO.toReplyEntity(entity);
@@ -64,43 +64,46 @@ public class ReplyService {
 	}
 
 
-//	public ReplyDTO update(ReplyDTO replyDTO) {
-//		  ReplyDTO existingReply = replyRepository.findById(); // 중복 조회 대신 replyDTO를 사용
-//		    
-//		    existingReply.setUsername(replyDTO.getUsername());
-//		    existingReply.setComment(replyDTO.getComment());
-//		    
-//		    ReplyEntity updatedReplyEntity = replyRepository.save(existingReply.toReplyEntity());
-//		    
-//	    return ReplyDTO.toReplyEntity(updatedReplyEntity);
-//	}
-
-	public ReplyDTO deleteById(long id) {
+	public void deleteById(long id) {
 		replyRepository.deleteById(id);
-		return null;
 	}
 
-//
-//	public ReplyDTO findById() {
-//		
-//		 Optional<ReplyEntity> replyEntity = replyRepository.findById();
-//		    
-//		    if (replyEntity == null) {
-//		        return null; 
-//		    }
-//		    
-//		    return ReplyDTO.toReplyDTO(replyEntity);
-//	}
-//
-//	public ReplyEntity update(ReplyDTO replyDTO) {
-//		 replyDTO = findById();
-//		 if(replyDTO == null) {
-//			 return null;
-//		 }
-//		 
-//		 replyDTO.setUsername(replyDTO.getUsername());
-//		 replyDTO.setComment(replyDTO.getComment());
-//		 
-//		return replyRepository.save(replyDTO.toReplyEntity());
-//	}
+	public List<ReplyDTO> findByBId(Long bid) {
+
+		List<ReplyEntity> list_entity = replyRepository.findByBid(bid);
+		List<ReplyDTO> list_dto = new ArrayList<>();
+
+		for (ReplyEntity e : list_entity) {
+			list_dto.add(ReplyDTO.toReplyEntity(e));
+		}
+
+		return list_dto;
+
+	}
+	public Page<ReplyDTO> findByBid(Long bid, int page) {
+		List<Sort.Order> sortList = new ArrayList<>();
+		sortList.add(Sort.Order.desc("id"));
+
+		Pageable pageable = PageRequest.of(page, 5, Sort.by(sortList));
+		Page<ReplyEntity> pageItem = replyRepository.findByBid(bid, pageable);
+		Page<ReplyDTO> pageDto = pageItem.map(entity -> new ReplyDTO(entity.getId(), entity.getBid(),
+				entity.getUsername(), entity.getComment(), entity.getCreateDate(), entity.getUpdateDate()
+, entity.getRole()
+		));
+		return pageDto;
+	}
+
+	public ReplyDTO findById(Long bid) {
+		Optional<ReplyEntity> optional = replyRepository.findById(bid);
+
+		if (!optional.isPresent()) {
+			throw new RuntimeException("잘못된 접근입니다. 댓글 id가 존재하지 않습니다.");
+		}
+
+		ReplyEntity replyEntity = optional.get();
+
+		return ReplyDTO.toReplyEntity(replyEntity);
+	}
+
+
 }
